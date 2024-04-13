3e6ffa0d29446501c76a49e288a9fe88371cbe47
JameEnder <jameender@email.cz>
2024-04-13 18:46:43 +0200

    Initial implementation

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/)
+import { Actor } from 'apify';
+// Crawlee - web scraping and browser automation library (Read more at https://crawlee.dev)
+// import { CheerioCrawler } from 'crawlee';
+
+// this is ESM project, and as such, it requires you to specify extensions in your relative imports
+// read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
+// note that we need to use `.js` even when inside TS files
+// import { router } from './routes.js';
+
+// The init() call configures the Actor for its environment. It's recommended to start every Actor with an init()
+await Actor.init();
+
+const client = Actor.newClient();
+
+// const { id } = await Actor.call('jameender/pokemon-scraper');
+
+const run = client.run("Kp80QfqUQ2bFsESEo");
+
+
+const dataset = run.dataset();
+const keyValue = run.apifyClient.keyValueStores()
+
+console.log(keyValue);
+
+
+
+// Gracefully exit the Actor process. It's recommended to quit all Actors with an exit()
+await Actor.exit();
